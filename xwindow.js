8b5fd6a2793f7d1acc04ee550df4a88e3c9d3536
John Alex <81481181+JohnAlexCO@users.noreply.github.com>
2022-05-01 02:41:00 -0400

    added in ANSI functions

--- xwindow.js
+++ xwindow.js
@@ -1,15 +1,22 @@
+module.exports = {
+	getEvents,
+	cursor,
+	clear,
+	color,
+	reset
+}
+
 const readline = require('readline');
 	readline.emitKeypressEvents(process.stdin);
 	process.stdin.setRawMode(true);
 
-const grabMouse = () => { process.stdout.write('\033[?1000h'); }
-const releaseMouse = () => { process.stdout.write('\033[?1000l'); }
-	
-// variables for grabbing mouse events
-	var moused = false;
-	var mouse_codes = [];
+const grabMouse = () => { escape('?1000h'); }
+const releaseMouse = () => { escape('1000l'); }
 
+var moused = false;
+var mouse_codes = [];
 const mousecodes = {' ':'Left', '!':'Middle', '"':'Right', '`':'Scroll-Up','a':'Scroll-Down'}
+
 function mouse_event(LATCHING_EVENT) {
 	var b = mousecodes[ mouse_codes[1] ]
 	var x = ( mouse_codes[2] ).charCodeAt(0)-33;
@@ -40,14 +47,12 @@ function key_event(LATCHING_EVENT, key) {
 		'event':'keypress',
 		'ctrl':key.ctrl,
 		'name':key.name,
-		'meta':key.meta,
 		'shift':key.shift,
 		'key':key.sequence
 	});
 	return;
 }
 
-module.exports = { getEvents }
 function getEvents( LATCHING_EVENT ) {
 	grabMouse();
 	process.stdin.on('keypress', (charater, key) => {
@@ -71,5 +76,45 @@ function getEvents( LATCHING_EVENT ) {
 		// other keys
 		key_event(LATCHING_EVENT, key);
 	})
+}
 
+function escape() {
+	var result = '\033[';
+	for (let i in arguments) {
+		result += arguments[i]
+		if (i != arguments.length-1) {
+			result += ';'
+		}
+	}
+	process.stdout.write(result)
+}
+
+function cursor(x,y) { 
+	escape(y, x+'H') 
+}	
+
+function clear() {
+	cursor(0,0)
+	escape('J')
+}
+
+function reset(){ escape('0m') }
+
+// asci colors
+function color(name, background=false){
+	const code = {
+		'black': 30,
+		'red': 31,
+		'green': 32,
+		'yellow': 33,
+		'blue': 34,
+		'magenta': 35,
+		'cyan': 36,
+		'white': 37,
+		'default': 39,
+		undefined: 39
+	}
+	var c = code[name]
+	if (background == true) { c+=10 }
+	escape(c+'m')
 }
